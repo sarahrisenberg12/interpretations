0614290b9e389b21bb8802a30c589776ae7c6bea
sarahrisenberg12 <55456099+sarahrisenberg12@users.noreply.github.com>
2019-09-30 17:34:14 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,325 @@
+function setup(){
+
+ createCanvas(700, 600)
+
+angleMode(DEGREES)
+
+strokeWeight(2)
+
+// head
+stroke(24, 89, 248)
+fill(24, 89, 248)
+beginShape()
+curveVertex(224, 151)
+curveVertex(224, 151)
+curveVertex(241, 107)
+curveVertex(281, 116)
+curveVertex(275, 163)
+curveVertex(275, 163)
+endShape()
+
+// leftarm
+stroke(24, 89, 248)
+beginShape()
+vertex(149, 143)
+vertex(103, 127)
+vertex(78, 53)
+vertex(83, 153)
+vertex(172, 195)
+endShape()
+
+// rightarm
+stroke(24, 89, 248)
+beginShape()
+vertex(343, 166)
+vertex(414, 215)
+vertex(299, 198)
+endShape()
+
+// torso
+stroke(24, 89, 248)
+beginShape()
+vertex(182, 221)
+vertex(96, 279)
+vertex(251, 292)
+vertex(272, 292)
+vertex(242, 237)
+endShape()
+
+
+// titties triangle
+fill(24, 89, 248)
+beginShape()
+vertex(343, 166)
+vertex(150, 143)
+vertex(202, 264)
+vertex(343, 166)
+// vertex(155, 122)
+// vertex(326, 182)
+// vertex(189, 262)
+// vertex(155, 122)
+endShape()
+
+// left leg
+beginShape()
+// vertex(96, 281)
+// vertex(117, 412)
+// vertex(162, 279)
+// vertex(96, 281)
+vertex(95, 280)
+vertex(110, 342)
+vertex(38, 335)
+vertex(107, 376)
+vertex(130, 376)
+vertex(161, 280)
+endShape()
+
+
+// right leg
+stroke(24, 89, 248) 
+beginShape()
+vertex(198, 289)
+vertex(219, 413)
+vertex(273, 294)
+vertex(198, 289)
+endShape()
+
+// torso
+stroke(51)
+strokeWeight(3)
+beginShape()
+vertex(160, 287)
+vertex(197, 290)
+vertex(259, 269)
+vertex(242, 239)
+vertex(298, 199)
+vertex(277, 159)
+vertex(234, 168)
+vertex(225, 153)
+vertex(155, 145)
+vertex(171, 197)
+vertex(180, 219)
+vertex(133, 255)
+vertex(161, 287)
+endShape()
+
+fill(217, 195, 137)
+beginShape()
+vertex(279, 165)
+vertex(265, 223)
+vertex(273, 219)
+vertex(289, 182)
+endShape()
+
+fill(255, 206, 202)
+beginShape()
+vertex(234, 170)
+vertex(197, 287)
+vertex(222, 281)
+vertex(262, 164)
+endShape()
+
+fill(253, 136, 3)
+beginShape()
+vertex(222, 154)
+vertex(170, 287)
+vertex(196, 291)
+vertex(235, 168)
+endShape()
+
+fill(253, 238, 3)
+beginShape()
+vertex(198, 150)
+vertex(160, 279)
+vertex(159, 288)
+vertex(170, 288)
+vertex(222, 155)
+endShape()
+
+fill(253, 16, 3)
+beginShape()
+vertex(156, 285)
+vertex(134, 258)
+vertex(175, 224)
+endShape()
+
+fill(253, 16, 3)
+beginShape()
+vertex(179, 213)
+vertex(158, 147)
+vertex(197, 150)
+endShape()
+
+fill(241, 76, 64)
+beginShape()
+vertex(262, 163)
+vertex(224, 282)
+vertex(257, 272)
+vertex(243, 241)
+vertex(266, 223)
+vertex(280, 165)
+vertex(278, 160)
+endShape()
+
+fill(254, 254, 254)
+beginShape()
+vertex(287, 181)
+vertex(273, 217)
+vertex(296, 200)
+endShape()
+
+line(292, 190, 284, 193)
+line(283, 194, 287, 207)
+
+
+strokeWeight(2)
+stroke(51)
+fill(252, 219, 216)
+circle(195, 180, 50)
+
+fill(126, 202, 96)
+circle(247, 198, 50)
+
+fill(253, 2, 2)
+circle(250, 187, 10)
+
+fill(245, 253, 2)
+circle(257, 193, 10)
+
+fill(253, 2, 2)
+circle(257, 203, 10)
+
+fill(245, 253, 2)
+circle(250, 212, 10)
+
+fill(253, 2, 2)
+circle(241, 210, 10)
+
+fill(253, 2, 2)
+circle(237, 194, 10)
+
+fill(245, 253, 2)
+circle(235, 203, 10)
+
+fill(245, 253, 2)
+circle(242, 189, 10)
+
+fill(225, 0, 0, 225)
+circle(246, 200, 15)
+
+
+// heart nipple
+fill(251, 76, 63)
+beginShape()
+curveVertex(194, 170)
+curveVertex(194, 170)
+curveVertex(186, 159)
+curveVertex(178, 165)
+curveVertex(182, 160)
+curveVertex(179, 164)
+curveVertex(182, 184)
+curveVertex(183, 198)
+curveVertex(200, 197)
+curveVertex(211, 189)
+curveVertex(216, 178)
+curveVertex(212, 170)
+curveVertex(206, 170)
+curveVertex(197, 172)
+curveVertex(196, 172)
+curveVertex(196, 172)
+
+// beginShape()
+// curveVertex(300, 98)
+// curveVertex(288, 98)
+// curveVertex(310, 100)
+// curveVertex(298, 69)
+// curveVertex(311, 44)
+// curveVertex(328, 42)
+// curveVertex(347, 42)
+// curveVertex(361, 52)
+// curveVertex(361, 63)
+// curveVertex(359, 82)
+// curveVertex(354, 92)
+// curveVertex(362, 103)
+// curveVertex(376, 104)
+// curveVertex(412, 105)
+// // curveVertex(412, 106)
+// curveVertex(431, 107)
+// curveVertex(451, 111)
+// curveVertex(477, 117)
+// curveVertex(491, 122)
+// curveVertex(494, 133)
+// curveVertex(450, 134)
+// curveVertex(450, 130)
+// curveVertex(450, 135)
+
+// curveVertex(432, 178)
+// curveVertex(434, 196)
+// curveVertex(442, 216)
+// curveVertex(434, 230)
+// curveVertex(421, 239)
+// curveVertex(420, 263)
+// curveVertex(420, 282)
+// curveVertex(421, 301)
+// curveVertex(424, 315)
+// curveVertex(418, 328)
+// curveVertex(409, 327)
+// curveVertex(400, 318)
+// curveVertex(390, 297)
+// curveVertex(388, 278)
+// curveVertex(388, 270)
+
+// curveVertex(374, 226)
+// curveVertex(360, 238)
+// curveVertex(355, 253)
+
+// curveVertex(336, 310)
+// // left leg
+// curveVertex(318, 340)
+// curveVertex(300, 340)
+// curveVertex(310, 300)
+// curveVertex(310, 300)
+// curveVertex(315, 283)
+// curveVertex(315, 270)
+// curveVertex(317, 258)
+// curveVertex(320, 246)
+// curveVertex(326, 231)
+// // left bum 
+// curveVertex(300, 222)
+// curveVertex(290, 215)
+// curveVertex(285, 211)
+// curveVertex(280, 195)
+// curveVertex(285, 175)
+// curveVertex(295, 153)
+// curveVertex(300, 141)
+// curveVertex(292, 133)
+// curveVertex(271, 111)
+// curveVertex(257, 100)
+// curveVertex(230, 82)
+// curveVertex(225, 73)
+// curveVertex(230, 65)
+// curveVertex(268, 62)
+// curveVertex(281, 91)
+// curveVertex(282, 92)
+// curveVertex(283, 95)
+// curveVertex(284, 96)
+// // left neck
+// curveVertex(295, 100)
+// curveVertex(300, 98)
+
+endShape()
+
+
+
+
+
+noFill()
+rect(0, 0, width, height)
+
+}
+
+function mouseClicked() {
+
+print(int(mouseX), int(mouseY))
+
+}
